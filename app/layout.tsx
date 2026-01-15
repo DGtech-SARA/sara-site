75c6452b8af86a5949fb0f1c08027832f922655d
DGtech-SARA <dgtechsara@gmail.com>
2026-01-15 11:42:27 -0300

    Adicionar fb:app_id para verificação do Facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,3 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "SARA - Sistema de Agendamento e Relacionamento Automatizado",
   description:
@@ -61,6 +47,8 @@ export default function RootLayout({
           name="facebook-domain-verification"
           content="rm0bih9htuwyjc6c0dy9ucmz0yoma9"
         />
+        {/* App ID do Facebook */}
+        <meta property="fb:app_id" content="1286834143331619" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
